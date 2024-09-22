763b80823dcfddf0320b2cedc80e86c822e2d096
Rakesh <rakkadam@cisco.com>
2024-09-22 14:49:22 +0530

    Adding heloworld

---
+++ src/main/java/com/rakkadam/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.rakkadam;
+
+/*
+ * @author rakkadam
+ * @createdAt 22/09/24
+ *
+ */public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
