d79d77da5687f49deabe8cac16ebe51a5b52c83e
Rakesh <rakkadam@cisco.com>
2024-09-22 14:41:03 +0530

    Init

---
+++ src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $org.rakeshsk;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
